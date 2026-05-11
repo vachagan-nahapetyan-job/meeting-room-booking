919b6bbfae9e67639e10b3ebecc70a4f45bd23a2
Vachagan Nahapetyan <vachagan.nahapetyan.job@gmail.com>
2026-05-11 14:44:07 +0400

    fix: auto-create .env from example file

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -3,27 +3,32 @@ set -e
 
 cd /var/www
 
-# Create required Laravel directories
 mkdir -p bootstrap/cache
 mkdir -p storage/framework/cache
 mkdir -p storage/framework/sessions
 mkdir -p storage/framework/views
 mkdir -p storage/logs
 
-# Install dependencies if vendor missing
+if [ ! -f .env ]; then
+    cp .env.example .env
+fi
+
 if [ ! -d "vendor" ]; then
     composer install --no-interaction --prefer-dist
 fi
 
-# Permissions
 chmod -R 775 storage bootstrap/cache
 chown -R www-data:www-data storage bootstrap/cache
 
-# Generate app key
 php artisan key:generate --force
 
-# Run migrations + seed
+echo "Waiting for database..."
+
+until php artisan migrate:status > /dev/null 2>&1
+do
+  sleep 2
+done
+
 php artisan migrate --force --seed
 
-# Start PHP-FPM
 exec php-fpm
